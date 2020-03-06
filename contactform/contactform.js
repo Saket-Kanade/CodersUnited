ca2f917bb43fb89f93d7174cfbfeb1bf179969a9
Ajay Bandagonda <bandagonda.ajay8@gmail.com>
2020-03-06 17:41:07 -0500

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,3 +1,34 @@
+var form = document.forms["contact"];
+    form.addEventListener('submit',contact_submit,false);
+  
+    function contact_submit(e) {
+      // Stop Form From Submitting
+      e.preventDefault();
+  
+      // Set Initial Variables
+      var target = e.target || e.srcElement;
+      var to = 'bandagonda.ajay8@gmail.com';
+      var uri = 'mailto:' + to;
+      var body = '';
+  
+      // Set Form Values to Variables
+      var name = target.elements['name'].value;
+      var subject = target.elements['subject'].value;
+      var email = target.elements['email'].value;
+      var message = target.elements['message'].value;
+  
+      // Build Body / Message with all Input Fields
+      body += message + "\r\n\r\n";
+      body += "Name: " + name + "\r\n";
+      body += "Email: " + email + "\r\n";
+  
+      // Build final Mailto URI
+      uri += '?subject=' + encodeURIComponent(subject);
+      uri += '&body=' + encodeURIComponent(body);
+  
+      // Open Mailto in New Window / Tab
+      window.open(uri,'_blank');
+    }
 // jQuery(document).ready(function($) {
 //   "use strict";
 
@@ -116,34 +147,3 @@
 //   });
 
 // });
-var form = document.forms["contact"];
-    form.addEventListener('submit',contact_submit,false);
-  
-    function contact_submit(e) {
-      // Stop Form From Submitting
-      e.preventDefault();
-  
-      // Set Initial Variables
-      var target = e.target || e.srcElement;
-      var to = 'bandagonda.ajay8@gmail.com';
-      var uri = 'mailto:' + to;
-      var body = '';
-  
-      // Set Form Values to Variables
-      var name = target.elements['name'].value;
-      var subject = target.elements['subject'].value;
-      var email = target.elements['email'].value;
-      var message = target.elements['message'].value;
-  
-      // Build Body / Message with all Input Fields
-      body += message + "\r\n\r\n";
-      body += "Name: " + name + "\r\n";
-      body += "Email: " + email + "\r\n";
-  
-      // Build final Mailto URI
-      uri += '?subject=' + encodeURIComponent(subject);
-      uri += '&body=' + encodeURIComponent(body);
-  
-      // Open Mailto in New Window / Tab
-      window.open(uri,'_blank');
-    }
